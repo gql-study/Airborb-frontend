e1c55fe11c27751e41cefb7c61ac364afb730661
Jinny <khj9709@icloud.com>
2022-06-24 02:45:05 +0900

    chore: add fonts

--- src/styles/globalStyle.ts
+++ src/styles/globalStyle.ts
@@ -1,8 +1,27 @@
 import { createGlobalStyle } from 'styled-components';
 import reset from 'styled-reset';
+import Medium from '../assets/fonts/AirbnbCereal_W_Md.otf';
+import Bold from '../assets/fonts/AirbnbCereal_W_Bd.otf';
+import Light from '../assets/fonts/AirbnbCereal_W_Lt.otf';
 
 const GlobalStyle = createGlobalStyle`
 	${reset}
+
+  @font-face {
+    font-family: 'Cereal';
+    src: url(${Medium}) format('opentype');
+    font-weight: 500;
+  }
+  @font-face {
+    font-family: 'Cereal';
+    src: url(${Bold}) format('opentype');
+    font-weight: 700;
+  }
+  @font-face {
+    font-family: 'Cereal';
+    src: url(${Light}) format('opentype');
+    font-weight: 300;
+  }
 
   #root, body, html {
     width: 375px;
