b88323abfae39e02f9dc4aba210ccae40f40e14e
soryeongk <soryeongk.kr@gmail.com>
2022-06-24 23:23:29 +0900

    feat: set asset and dummy data

---
+++ src/assets/svgIcons/images/GalleryImages.ts
@@ -0,0 +1,44 @@
+const MOCK_IMAGE_LIST = [
+  {
+    url: 'https://dummyimage.com/720x960/FF5A5F/fff.png&text=%20%20AirBorB01',
+    alt: '더미 사진01',
+  },
+  {
+    url: 'https://dummyimage.com/720x960/00A699/fff.png&text=%20%20AirBorB02',
+    alt: '더미 사진02',
+  },
+  {
+    url: 'https://dummyimage.com/720x960/FC642D/fff.png&text=%20%20AirBorB03',
+    alt: '더미 사진03',
+  },
+  {
+    url: 'https://dummyimage.com/720x960/484848/fff.png&text=%20%20AirBorB04',
+    alt: '더미 사진04',
+  },
+  {
+    url: 'https://dummyimage.com/720x960/767676/fff.png&text=%20%20AirBorB05',
+    alt: '더미 사진05',
+  },
+  {
+    url: 'https://dummyimage.com/720x960/FF5A5F/fff.png&text=%20%20AirBorB06',
+    alt: '더미 사진06',
+  },
+  {
+    url: 'https://dummyimage.com/720x960/00A699/fff.png&text=%20%20AirBorB07',
+    alt: '더미 사진07',
+  },
+  {
+    url: 'https://dummyimage.com/720x960/FC642D/fff.png&text=%20%20AirBorB08',
+    alt: '더미 사진08',
+  },
+  {
+    url: 'https://dummyimage.com/720x960/484848/fff.png&text=%20%20AirBorB09',
+    alt: '더미 사진09',
+  },
+  {
+    url: 'https://dummyimage.com/720x960/767676/fff.png&text=%20%20AirBorB10',
+    alt: '더미 사진10',
+  },
+];
+
+export default MOCK_IMAGE_LIST;
