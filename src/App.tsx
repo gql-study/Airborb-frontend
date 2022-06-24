2c53a46108104119684aefe69945401e3bb5d369
soryeongk <soryeongk.kr@gmail.com>
2022-06-24 21:39:03 +0900

    feat: set page router

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,16 @@
-import React from 'react';
 import { ApolloProvider } from '@apollo/client';
 import { ThemeProvider } from 'styled-components';
 import GlobalStyle from './styles/globalStyle';
 import client from './apollo/client';
 import theme from './styles/theme';
+import Router from './pages/Router';
 
 function App() {
   return (
     <ApolloProvider client={client}>
       <ThemeProvider theme={theme}>
         <GlobalStyle />
+        <Router />
       </ThemeProvider>
     </ApolloProvider>
   );
